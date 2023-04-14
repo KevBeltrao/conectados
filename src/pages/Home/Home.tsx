8a9209cf62a868c2a69cce9e338b25e8f724ccaa
Kevin Beltrao de Melo <kbmelo@outlook.com>
2023-04-14 13:59:13 -0300

    feature(quest-card): removing example before merge

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import { type FC } from 'react';
 
-import QuestCard from '../../components/QuestCard';
-
 import { Container } from './styles';
 
 interface HomeTypes {
@@ -11,37 +9,6 @@ interface HomeTypes {
 const Home: FC<HomeTypes> = () => (
   <Container>
     <h1>Home</h1>
-
-    <div
-      style={{
-        padding: 10,
-        position: 'relative',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 24,
-      }}
-    >
-      <QuestCard
-        status="answered"
-        questName="Quest 1"
-        title="Você sabe?"
-        targetUrl="/quest1"
-      />
-
-      <QuestCard
-        status="pending"
-        questName="Quest 2"
-        title="Complete a letra"
-        targetUrl="/quest2"
-      />
-      <QuestCard
-        status="disabled"
-        questName="Quest 3"
-        title="Hora da selfie"
-        targetUrl="/quest3"
-      />
-    </div>
   </Container>
 );
 
