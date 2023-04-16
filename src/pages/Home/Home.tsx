cd3c708b2a454e1a22ab174b6b39b4727ff1317d
zehzo <edson.ferreiraj@hotmail.com>
2023-04-16 12:30:23 -0300

    fix(update-button-style): remove example before merge
    #09

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import { type FC } from 'react';
 
-import Button from '../../components/Button';
-
 import { Container } from './styles';
 
 interface HomeTypes {
@@ -12,23 +10,6 @@ const Home: FC<HomeTypes> = () => {
   return (
     <Container>
       <h1>Home</h1>
-      <Button
-        onClick={() => {
-          // eslint-disable-next-line no-alert
-          alert('Entrou');
-        }}
-      >
-        Click me
-      </Button>
-      <Button
-        disabled
-        onClick={() => {
-          // eslint-disable-next-line no-alert
-          alert('Entrou');
-        }}
-      >
-        Click me
-      </Button>
     </Container>
   );
 };
